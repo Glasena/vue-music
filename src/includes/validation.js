965d7504e2f47707061ac3cd27c065f5c213b957
Glasena <alisonglasenapp@gmail.com>
2025-12-28 21:44:25 -0300

    Added doc for clarification

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,6 +1,7 @@
 import { Form as VeeForm, Field as VeeField } from "vee-validate"
 
 export default {
+    //esse install ai é coisa do vue, pra registrar plugin
     install(app) {
         app.component("VeeForm", VeeForm),
         app.component("VeeField", VeeField)

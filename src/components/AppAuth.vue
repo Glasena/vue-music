3152b31f60e919996f1bc55656479dbc13fcc944
Glasena <alisonglasenapp@gmail.com>
2025-12-30 19:54:00 -0300

    Reproduzir 110. Additional Rules

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -169,7 +169,7 @@ export default {
     return {
       tab: 'login',
       schema: {
-        name: 'required',
+        name: 'required|min:3|max:100|alpha_spaces',
         email: '',
         age: '',
         password: '',

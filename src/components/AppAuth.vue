402c62b93185378894ce25c54d08f73633126e81
Glasena <alisonglasenapp@gmail.com>
2025-12-30 19:48:11 -0300

    109. Applying Rules

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -81,7 +81,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -91,6 +91,8 @@
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <!-- O nome do componente aqui tem que ser o nome do que vai ser validado -->
+              <ErrorMessage class="text-red-600" name="name"></ErrorMessage>
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -159,12 +161,22 @@
 //mapState só lê, mapWritableState lê e altera
 import { mapState, mapWritableState } from 'pinia'
 import useModalStore from '@/stores/modal'
+import { ErrorMessage } from 'vee-validate'
 
 export default {
   name: 'AppAuth',
   data() {
     return {
       tab: 'login',
+      schema: {
+        name: 'required',
+        email: '',
+        age: '',
+        password: '',
+        confirm_password: '',
+        country: '',
+        tos: '',
+      },
     }
   },
   computed: {

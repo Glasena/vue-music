6c3e16fad43cfd9bcc1fca61bbf66ad1b6cbaa7b
Glasena <alisonglasenapp@gmail.com>
2025-12-30 21:19:50 -0300

    117. Rendering Multiple Error Messages

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -118,12 +118,16 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field
-                type="password"
-                name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
+              <vee-field type="password" name="password" :bails="false" v-slot="{ field, errors }"
+                ><input
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div></vee-field
+              >
               <ErrorMessage class="text-red-600" name="password"></ErrorMessage>
             </div>
             <!-- Confirm Password -->
@@ -190,7 +194,7 @@ export default {
         name: 'required|min:3|max:100|alpha_spaces',
         email: 'required|min:3|max:100|email',
         age: 'required|min_value:18|max_value:120|email',
-        password: 'required|min:3|max:100',
+        password: 'required|min:9|max:100|excluded:password',
         confirm_password: 'confirmed:@password',
         country: 'required|excluded:Antarctica',
         tos: 'required',

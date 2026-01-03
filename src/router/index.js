d3db87ee7880722aafae3670dbbb782d8abd7171
Glasena <alisonglasenapp@gmail.com>
2026-01-03 09:07:32 -0300

    143. Creating Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
+import AboutView from '@/views/AboutView.vue';
+import HomeView from '@/views/HomeView.vue';
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: "/",
+    component: HomeView
+  },
+  {
+    path: "/about",
+    component: AboutView
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router

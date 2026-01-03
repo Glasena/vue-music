2a19c002dccc4dd88cdf0a5553bcf0d5357a34f8
Glasena <alisonglasenapp@gmail.com>
2026-01-02 21:51:29 -0300

    138. Setting up the Login

--- src/stores/user.js
+++ src/stores/user.js
@@ -8,21 +8,24 @@ export default defineStore("user", {
     }),
     actions: {
         async register(values) {
-        const userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
-        await usersCollection.doc(userCred.user.uid).set({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        })
+            const userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
+            await usersCollection.doc(userCred.user.uid).set({
+            name: values.name,
+            email: values.email,
+            age: values.age,
+            country: values.country,
+            })
 
-        await userCred.user.updateProfile({
-            displayName: values.name
-        })
+            await userCred.user.updateProfile({
+                displayName: values.name
+            })
 
-        this.userLoggedIn = true
+            this.userLoggedIn = true
+        },
+        async authenticate(values) {
+            await auth.signInWithEmailAndPassword(values.email, values.password);
 
-
+            this.userLoggedIn = true
         }
     }
 });
